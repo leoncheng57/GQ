c6b28257ff2f58d6ad18957115059ab545d3acd9
Masa13 <masahero55@gmail.com>
2015-11-19 13:13:53 -0500

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 import os.path, google, urllib2, bs4, re
 
 app = Flask(__name__)
@@ -10,18 +10,21 @@ def index():
 @app.route("/search", methods=["GET","POST"])
 @app.route("/search/", methods=["GET","POST"])
 def gSearch():
-    results = google.search(q, num=10,start=0,stop=10)
-    rlist = []
-    for r in results:
-        rlist.append(r)
-    url = urllib2.urlopen(rlist[0])
-    page = url.read()
-    soup = bs4.BeautifulSoup(page,'lxml')
-    raw = soup.get_text()
-
-    text = re.sub("[ \t\n]+"," ", raw)
-    print text
-    #return render_template("results.html",result=test)
+    if request.method == "GET":
+        return render_template("search.html")
+    else:
+        results = google.search(q, num=10,start=0,stop=10)
+        rlist = []
+        for r in results:
+            rlist.append(r)
+        url = urllib2.urlopen(rlist[0])
+        page = url.read()
+        soup = bs4.BeautifulSoup(page,'lxml')
+        raw = soup.get_text()
+    
+        text = re.sub("[ \t\n]+"," ", raw)
+        #print text
+        return render_template("results.html",result=test)
 
 if __name__ == '__main__':
     app.debug = True
