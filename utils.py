fbf95f1aa259584b134804fd4bafd57384dc9937
Masa13 <masahero55@gmail.com>
2015-11-25 13:19:56 -0500

    added utils.py

---
+++ utils.py
@@ -0,0 +1,21 @@
+import re
+
+#List of RegEx Expression (Patterns)
+#WHO:Name Patterns
+p1 = ""
+
+#WHEN: Number Patterns
+#Date
+
+
+#Phone Number
+pattern = "[0-9]{3}-[0-9]{3}-[0-9]{4}"
+
+def find_name(s):
+    """Returns a list of valid names given a string of text"""
+
+def find_date(s):
+    """Returns a list of valid dates given a string of text"""
+    if __name__ == "__main__":
+        import doctest
+        doctest.testmod()
